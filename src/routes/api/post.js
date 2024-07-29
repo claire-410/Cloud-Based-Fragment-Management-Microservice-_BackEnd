af560ceb48ce5df81470c3fe3a8fe48a5803e43f
Rezi <reziyemu.sulaiman@gmail.com>
2024-07-29 19:50:39 -0400

    assignment3-update2

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,41 +1,31 @@
 // src/routes/api/post.js
 
-const {createSuccessResponse, createErrorResponse} = require('../../response');
-const {Fragment} = require('../../model/fragment');
-const logger = require('../../logger');
-
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+// const express = require('express');
+const { Fragment } = require('../../model/fragment');
+// const contentType = require('content-type');
 const API_URL = process.env.API_URL;
 
-module.exports = async (req, res)=>{
-    logger.debug('Post: ' + req.body);
+// const logger = require('../../logger');
 
-    if(!Buffer.isBuffer(req.body)) {
-        return res.status(415).json(createErrorResponse(400, 'The media type is not supported.'));
-    }
-
+module.exports = async (req, res) => {
+  if (!Buffer.isBuffer(req.body)) {
+    res.status(415).json(createErrorResponse(415, 'Type error'));
+  } else {
     try {
-        const fragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type') });
-        await fragment.save();
-        await fragment.setData(req.body);
-    
-        logger.debug('New fragment created: ' + JSON.stringify(fragment));
-    
-        res.set('Content-Type', fragment.type);
-       // res.set('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
-        res.set('Location', API_URL + '/v1/fragments/' + fragment.id);
-        res.status(201).json(
-          createSuccessResponse({
-            fragment: fragment,
-          })
-        );
-      } catch (err) {
-        res.status(500).json(createErrorResponse(500, err));
+      const frag = new Fragment({ ownerId: req.user, type: req.get('Content-Type') });
+      await frag.save();
+      await frag.setData(req.body);
+      res.set('Location', API_URL + '/v1/fragments/' + frag.id);
+      res.set('Content-Type', frag.type);
+
+      res.status(201).json(
+        createSuccessResponse({
+          fragment: frag,
+        })
+      );
+    } catch (error) {
+      res.status(400).json(createErrorResponse(400, error));
     }
+  }
 };
-
-
-
-
-
-
-
